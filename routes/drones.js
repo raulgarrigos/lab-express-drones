f8cb16f019b8540624c11ac3c7a2b4c649a91c53
Adrian Siquier <adriansiquiervalles@gmail.com>
2023-11-06 17:45:53 +0100

    iteration 2 and 3 done

--- routes/drones.js
+++ routes/drones.js
@@ -1,34 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Drone = require("../models/Drone.model");
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // Iteration #2: List the drones
+  try {
+    const response = await Drone.find();
+    console.log(response);
+    res.render("drones/list.hbs", {
+      allDrones: response,
+    });
+  } catch (err) {
+    next(err);
+  }
   // ... your code here
 });
 
-router.get('/drones/create', (req, res, next) => {
+// GET "/drones/create"
+router.get("/create", (req, res, next) => {
   // Iteration #3: Add a new drone
+  res.render("drones/create-form.hbs");
   // ... your code here
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post("/create", async (req, res, next) => {
   // Iteration #3: Add a new drone
+  try {
+    await Drone.create({
+      name: req.body.name,
+      propellers: req.body.propellers,
+      maxSpeed: req.body.maxSpeed,
+    });
+    res.redirect("/drones");
+  } catch (err) {
+    next(err);
+  }
   // ... your code here
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
 });
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ const router = express.Router();
 /* GET home page */
 router.get("/", (req, res, next) => res.render("index"));
 
+const droneRouter = require("./drones.js");
+router.use("/drones", droneRouter);
+
 module.exports = router;
